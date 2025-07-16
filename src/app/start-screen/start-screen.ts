54d38a1c73e711c9b6d15aae4318b362aa0332b2
ostermann-software <ostermann-software@gmx.de>
2025-07-16 06:01:30 +0200

    Karten + startscreen

---
+++ src/app/start-screen/start-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen',
+  imports: [],
+  templateUrl: './start-screen.html',
+  styleUrl: './start-screen.scss'
+})
+export class StartScreen {
+
+}
